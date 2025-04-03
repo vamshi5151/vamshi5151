d5d686a23726d774175063295b8b3e939f26c779
vamshi5151 <profvamshi@gmail.com>
2025-04-03 19:34:26 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple alert for interactivity
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Resume page loaded!");
+    alert("Welcome to my resume website!");
+});
